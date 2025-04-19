5e303099d7d0e9a782b2ca49feb31d9b0246a7bd
JCoelho13 <jantonioboavista@gmail.com>
2025-04-19 14:41:21 +0100

    added new vars

--- bin.py
+++ bin.py
@@ -5,16 +5,17 @@ from spade.agent import Agent
 class Bin(Agent):
     
     bin_number = 1
-    waste_lvl = 10
+    current_waste_lvl = 0
+    capacity = 20
     name = "bin"
     is_manager = False     
     current_message_type = ""
         
     async def setup(self):
         self.variables = {
-            "waste_lvl": self.waste_lvl,  
+            "current_waste_lvl": self.current_waste_lvl,  
         }
-        self.announcements = [Announcement(AnnouncementType.SET_VARIABLE_LESS_THAN_OR_EQUAL, "waste_lvl", 5)]
+        self.announcements = [Announcement(AnnouncementType.SET_VARIABLE_LESS_THAN_OR_EQUAL, "current_waste_lvl", 5)]
         self.name = self.name + str(Bin.bin_number)
         Bin.bin_number += 1
         # Called when the agent starts
