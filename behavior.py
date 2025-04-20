62e08a4610d35722bbf0e40bba7a2d23b52eb1b6
diogotvf7 <diogotvf7@gmail.com>
2025-04-20 23:22:01 +0100

    Main menu

--- behavior.py
+++ behavior.py
@@ -5,72 +5,100 @@ from announcement_types import AnnouncementType
 from central import central
 import asyncio
 
-STATE_ONE = "CALL_FOR_PROPOSAL"
-STATE_TWO = "REFUSE"
-STATE_THREE = "PROPOSE"
-STATE_FOUR = "ACCEPT_PROPOSAL"
-STATE_FIVE = "INFORM:DONE"
-STATE_SIX = "FAILURE"
+STATE_ONE = "CALL_FOR_PROPOSAL"  # Bin is full
+STATE_TWO = "FAILURE"  # Every trucks refuse
+STATE_THREE = "PROPOSE"  # Truck proposes to pick up the bin
+STATE_FOUR = "ACCEPT_PROPOSAL"  # Bin accepts the proposal
+STATE_FIVE = "INFORM:DONE"  # Bin informs the truck that the job is done
+# STATE_SIX = "FAILURE"  # Bin informs the truck that the job failed
 
 
 class EmptyGarbage(CyclicBehaviour):
-        async def on_start(self):
-            print("Starting announcement . . .")
-            
-        async def run(self):
-            fsm = ContractNetFSMBehaviour()
-            fsm.add_state(name=STATE_ONE, state=StateOne(), initial=True)
-            fsm.add_state(name=STATE_TWO, state=StateTwo())
-            fsm.add_state(name=STATE_THREE, state=StateThree())
-            fsm.add_transition(source=STATE_ONE, dest=STATE_TWO)
-            fsm.add_transition(source=STATE_ONE, dest=STATE_THREE)
-            fsm.add_transition(source=STATE_THREE, dest=STATE_TWO)
-            fsm.add_transition(source=STATE_THREE, dest=STATE_FOUR)
-            
-            
-            self.agent.current_msg_type = AnnouncementType.SET_VARIABLE_LESS_THAN_OR_EQUAL.value
+    async def on_start(self):
+        print("Starting announcement . . .")
 
-            self.agent.add_behaviour(fsm)
-            await asyncio.sleep(5)
-            
-class StateOne(State):
     async def run(self):
-        print("I'm at state one")
-        for key, item in central.contractors.items():    
+        fsm = ContractNetFSMBehaviour()
+        fsm.add_state(name=STATE_ONE, state=StateOne(), initial=True)
+        fsm.add_state(name=STATE_TWO, state=StateTwo())
+        fsm.add_state(name=STATE_THREE, state=StateThree())
+        fsm.add_transition(source=STATE_ONE, dest=STATE_TWO)
+        fsm.add_transition(source=STATE_ONE, dest=STATE_THREE)
+        fsm.add_transition(source=STATE_THREE, dest=STATE_TWO)
+        fsm.add_transition(source=STATE_THREE, dest=STATE_FOUR)
+
+        self.agent.current_msg_type = (
+            AnnouncementType.SET_VARIABLE_LESS_THAN_OR_EQUAL.value
+        )
+
+        self.agent.add_behaviour(fsm)
+        await asyncio.sleep(5)
+
+
+class StateOne(State):
+    """
+    Bin is full - Call for trucks proposals
+    """
+
+    async def run(self):
+        print("1️⃣ Bin is full - Call for trucks proposals")
+        for key, item in central.contractors.items():
             msg = Message(to=str(key))
-            msg.set_metadata("type", AnnouncementType.SET_VARIABLE_LESS_THAN_OR_EQUAL.value)
+            msg.set_metadata(
+                "type", AnnouncementType.SET_VARIABLE_LESS_THAN_OR_EQUAL.value
+            )
             msg.set_metadata("requirements", "waste_lvl<=5")
             msg.set_metadata("deadline", "5")
-            msg.set_metadata("eval_criteria", "waste_lvl_before,waste_lvl_after,distance")
+            msg.set_metadata(
+                "eval_criteria", "waste_lvl_before,waste_lvl_after,distance"
+            )
             msg.body = f"Need to drop waste from bin {self.agent.bin_number}."
-            self.set_next_state(STATE_TWO) if not item.isAvailable() else self.set_next_state(STATE_THREE)         
-                
-        
+            (
+                self.set_next_state(STATE_TWO)
+                if not item.isAvailable()
+                else self.set_next_state(STATE_THREE)
+            )
+
 
 class StateTwo(State):
+    """
+    No trucks available - all refused call for proposals
+    """
+
     async def run(self):
-        print("I REFUSE")
-        
+        print("2️⃣ No trucks available - all refused call for proposals")
+
 
 class StateThree(State):
+    """
+    Truck proposes to pick up the bin
+    """
+
     async def run(self):
-        print("I PROPOSE")
+        print("3️⃣ Truck proposes to pick up the bin")
         msg = Message(to=str(self.agent.jid))
         msg.set_metadata("type", "Proposal")
         msg.set_metadata("", "Proposal")
-        
+
+
 class StateFour(State):
+    """
+    Bin accepts the proposal
+    """
+
     async def run(self):
-        print("I ACCEPT PROPOSAL")
-        
+        print("4️⃣ Bin accepts the proposal")
+
+
 class StateFive(State):
+    """
+    Bin informs the truck that the job is done
+    """
+
     async def run(self):
-        print("INFORM:DONE")
-        
-class StateSix(State):
-    async def run(self):
-        print("FAILURE")
+        print("5️⃣ Bin informs the truck that the job is done")
+
 
 class ContractNetFSMBehaviour(FSMBehaviour):
     async def on_start(self):
-        print("FSM starting")
+        print("--------")
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import getpass
 import spade
 import asyncio
-#from spade_bdi.bdi import BDIAgent
+import random
+import copy
+
+# from spade_bdi.bdi import BDIAgent
 from central import central
 from bin import Bin
 from truck import Truck
@@ -11,100 +14,155 @@ from spade import wait_until_finished
 from behavior import EmptyGarbage
 from node import Node
 from edge import Edge
+from simple_term_menu import TerminalMenu
 
-nodes = {}
-edges = {}
+NODES = {}
+EDGES = {}
 
 
+def main():
+    while True:
+        options = ["Scenario 1", "Scenario 2", "Scenario 3"]
+        terminal_menu = TerminalMenu(options)
+        menu_entry_index = terminal_menu.show()
+        selection = options[menu_entry_index]
 
-async def main():
-    
-    # First scenario hardcoded 2 trucks and 2 bins
-    
+        if selection == "Scenario 1":
+            spade.run(scenario_1())
+        elif selection == "Scenario 2":
+            spade.run(scenario_2())
+        elif selection == "Scenario 3":
+            spade.run(scenario_3())
+
+
+async def scenario_1():
+    """
+    Scenario 1:
+    • Bin filling rate (time interval): every 300 seconds (ensure that the bin fills up once and only
+    once)
+    • Bin filling rate (quantity): 100% of the bin capacity
+    • Number of Trucks: 1
+    • End of simulation: 240 seconds
+    """
     truck_agent10 = Truck("asma@draugr.de/10", "1234")
     await truck_agent10.start(auto_register=True)
     await asyncio.sleep(1)
     truck_agent10.setContractor(central)
-    #print(f"Is {truck_agent.name} a contractor? {truck_agent.getContractor()}")
-    
+    NODES["X"].truck = truck_agent10
+
+    nodes = get_rand_bins(list(NODES.values()), 6)
+
+    for node in nodes:
+        await node.bin.start(auto_register=True)
+        await asyncio.sleep(1)
+        node.bin.setManager(central)
+
+        await wait_until_finished(node.bin)
+
+
+def gen_bins():
+    behavior = EmptyGarbage()
+    for i, bin in zip(range(16), "ABCDEFGHIJKLMNOP"):
+        bin_agent = Bin(f"asma@draugr.de/{i}", "1234")
+        bin_agent.add_behaviour(behavior)
+        NODES[bin].bin = bin_agent
+
+
+def get_rand_bins(bins, n):
+    return random.sample(bins, n)
+
+
+async def _():
+
+    # First scenario hardcoded 2 trucks and 2 bins
+
+    truck_agent10 = Truck("asma@draugr.de/10", "1234")
+    await truck_agent10.start(auto_register=True)
+    await asyncio.sleep(1)
+    truck_agent10.setContractor(central)
+    # print(f"Is {truck_agent.name} a contractor? {truck_agent.getContractor()}")
+
     truck_agent11 = Truck("asma@draugr.de/11", "1234")
     await truck_agent11.start(auto_register=True)
     await asyncio.sleep(1)
     truck_agent11.setContractor(central)
-    #print(f"Is {truck_agent.name} a contractor? {truck_agent.getContractor()}")
-    
+    # print(f"Is {truck_agent.name} a contractor? {truck_agent.getContractor()}")
+
     bin_agent0 = Bin("asma@draugr.de/0", "1234")
     await bin_agent0.start(auto_register=True)
-    await asyncio.sleep(1)  
+    await asyncio.sleep(1)
     bin_agent0.setManager(central)
-    #print(f"Is {bin_agent.name} a manager? {bin_agent.getManager()}")
-    
+    # print(f"Is {bin_agent.name} a manager? {bin_agent.getManager()}")
+
     bin_agent1 = Bin("asma@draugr.de/1", "1234")
     await bin_agent1.start(auto_register=True)
-    await asyncio.sleep(1)  
+    await asyncio.sleep(1)
     bin_agent1.setManager(central)
-    #print(f"Is {bin_agent.name} a manager? {bin_agent.getManager()}")
-    
-    
-    
-    
-    
-    # Add agents to graph 
-    
-    nodes['X'].truck = truck_agent10
-    nodes['A'].truck = truck_agent11
-    nodes['L'].bin = bin_agent0
-    nodes['M'].bin = bin_agent1
-    
-    
+    # print(f"Is {bin_agent.name} a manager? {bin_agent.getManager()}")
+
+    # Add agents to graph
+
+    NODES["X"].truck = truck_agent10
+    NODES["A"].truck = truck_agent11
+    NODES["L"].bin = bin_agent0
+    NODES["M"].bin = bin_agent1
+
     # Behavior empty garbage
-    
+
     behavior1 = EmptyGarbage()
     bin_agent0.add_behaviour(behavior1)
     bin_agent1.add_behaviour(behavior1)
-    
 
     await wait_until_finished(bin_agent0)
     await wait_until_finished(bin_agent1)
-    
 
-if __name__ == "__main__":
-    
-    f = open('graph_files/proj1_edgelist', 'r')
+
+async def scenario_2():
+    # Scenario 2 logic here
+    print("Executing Scenario 2...")
+    # You can call the main function or any other function here
+    await main()
+
+
+async def scenario_3():
+    # Scenario 3 logic here
+    print("Executing Scenario 3...")
+    # You can call the main function or any other function here
+    await main()
+
+
+def read_graph():
+    f = open("graph_files/proj1_edgelist", "r")
     a = f.readlines()
     nodes_set = set()
     inc_out_set = set()
     for line in a:
-        
         splited = line.split()
         nodes_set.add(splited[0])
         inc_out_set.add((splited[0], splited[1], splited[2]))
-        
-        
+
     for node in nodes_set:
         node_ = Node(node)
-        nodes[node] = node_ 
-    
-    nodes['X'] = Node('X')
-        
-    
-    for node_pair in inc_out_set:
-       edge = Edge(node_pair[0], node_pair[1], node_pair[2])
-       edges[(node_pair[0],node_pair[1])] = edge
+        NODES[node] = node_
 
-    for key,value in edges.items():
-        nodes[key[0]].edge_list.add(value)
-        nodes[key[1]].edge_list.add(value)
-    
-    
-    for key,value in nodes.items():
-        print("--------")
-        for edge in value.edge_list:
-            print(edge.A, edge.B)
-    
-    
-    # For first scenario
-    
-    #nodes['A'].bin = 
-    
-    spade.run(main())
+    NODES["X"] = Node("X")
+
+    for node_pair in inc_out_set:
+        edge = Edge(node_pair[0], node_pair[1], node_pair[2])
+        EDGES[(node_pair[0], node_pair[1])] = edge
+
+    for key, value in EDGES.items():
+        NODES[key[0]].edge_list.add(value)
+        NODES[key[1]].edge_list.add(value)
+
+
+if __name__ == "__main__":
+
+    # Read the graph from the file and populate NODES and EDGES
+    read_graph()
+
+    # Initialize read bins from graph
+    gen_bins()
+
+    # Run the menu function
+    main()
