b8949513c10d42af6cabeba98da4978c9893cb1e
Válter Castro <valter@Valters-MacBook-Pro-2.local>
2025-04-20 14:11:48 +0100

    world representation; 1st scenario setup

---
+++ edge.py
@@ -0,0 +1,6 @@
+class Edge():
+    
+    def __init__(self, A, B, weight):
+        self.A = A
+        self.B = B
+        self.weight = weight
---
+++ node.py
@@ -0,0 +1,9 @@
+class Node():
+    
+    truck = None
+    bin = None
+    
+
+    def __init__(self,name):
+        self.name = name        
+        self.edge_list = set()
